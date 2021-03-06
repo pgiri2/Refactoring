fcd3604da954b78a7a037f22ade36e40434e1db7
14022 <14022@LAPTOP-VBLT9I6N>
2021-03-05 18:09:08 -0600

    ex-0302

---
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -0,0 +1,36 @@
+package q1.extract_method.refactored;
+
+import java.util.List;
+
+public class A {
+   Node m1(List<Node> nodes, String p) {
+      // TODO: Your answer
+	   ex(nodes,p);
+      // other implementation
+      return null;
+   }
+
+   Edge m2(List<Edge> edgeList, String p) {
+      // TODO: Your answer
+	   ex(edgeList,p);
+      // other implementation
+      return null;
+   }
+
+   <T extends Graph> void ex(List<T> objs, String p)
+   {
+	   for(T obj: objs)
+	   {
+		   if(obj.contains(p))
+			   System.out.println(obj);
+	   }
+   }
+   // TODO: Your answer
+   // ? extractedMethod(?) {
+   //
+   // }
+}
+
+class Node extends Graph{}
+
+class Edge extends Graph{}
